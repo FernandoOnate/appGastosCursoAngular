3f7ffe59755fd74a9b8f2f61ffe35f96ecf086b5
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-02-22 01:34:47 -0500

    - Añadidos componentes de ingresos, egresos y formulario - Plantillas html listas - app module listo - app component listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CabeceroComponent } from './cabecero/cabecero.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { IngresosComponent } from './ingresos/ingresos.component';
+import { EgresosComponent } from './egresos/egresos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceroComponent
+    CabeceroComponent,
+    FormularioComponent,
+    IngresosComponent,
+    EgresosComponent
   ],
   imports: [
     BrowserModule
